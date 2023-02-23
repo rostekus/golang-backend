86289af504195de3efd20f90788fd49b67e149b4
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-02-23 23:48:39 +0100

    feat: moved db configs to sapare files

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"time"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -16,21 +17,6 @@ type PostgresDatabase struct {
 }
 
 type MongoDatabase struct {
-	client *mongo.Client
-}
-
-type dbPostgresConfig struct {
-	User     string `mapstructure:"POSTGRES_USER" validate:"required"`
-	Password string `mapstructure:"POSTGRES_PASSWORD" validate:"required"`
-	Host     string `mapstructure:"POSTGRES_HOST" validate:"required"`
-	Name     string `mapstructure:"POSTGRES_DB" validate:"required"`
-	Port     string `mapstructure:"POSTGRES_PORT"`
-}
-
-type dbMongoConfig struct {
-	User     string `mapstructure:"MONGO_INITDB_ROOT_USERNAME" validate:"required"`
-	Password string `mapstructure:"MONGO_INITDB_ROOT_PASSWORD" validate:"required"`
-	Host     string `mapstructure:"MONGO_HOST" validate:"required"`
-	Port     string `mapstructure:"MONGO_PORT"`
-	Name     string `mapstructure:"MONGO_INITDB_DATABASE" validate:"required"`
+	client  *mongo.Client
+	timeout time.Duration
 }
